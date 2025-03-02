ba0916fb44bae0e821b525c3beb17c1d4368a1df
Lzlzy <59407766+lzlzyZZ@users.noreply.github.com>
2025-03-02 16:20:05 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -38,7 +38,11 @@ ifnames=$(echo "$ifnames" | awk '{$1=$1};1')
 if [ "$count" -eq 1 ]; then
    # 单网口设备 类似于NAS模式 动态获取ip模式 具体ip地址取决于上一级路由器给它分配的ip 也方便后续你使用web页面设置旁路由
    # 单网口设备 不支持修改ip 不要在此处修改ip 
-   uci set network.lan.proto='dhcp'
+   # LAN口设置静态IP
+   uci set network.lan.proto='static'
+   uci set network.lan.ipaddr='192.168.0.2'
+   uci set network.lan.netmask='255.255.255.0'
+   echo "set 192.168.100.1 at $(date)" >> $LOGFILE
 elif [ "$count" -gt 1 ]; then
    # 提取第一个接口作为WAN
    wan_ifname=$(echo "$ifnames" | awk '{print $1}')
